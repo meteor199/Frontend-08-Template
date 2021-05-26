0daae79ea3ff2ef37fc0fdc29170dad8a1e61e17
meteor <meteor199@qq.com>
2021-05-26 19:42:55 +0800

    init

---
+++ Week 20/headless-demo/main.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('http://localhost:8080/main.html');
+
+  const a = await page.$('a');
+  const img = await page.$$('a');
+
+  console.log(await a.asElement().boxModel());
+  console.log(await img.asElement().boxModel());
+})();
